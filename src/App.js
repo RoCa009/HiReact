b35dc43da0659c8e1b4e77ca6e26abd630cda6c8
Rodrigo Caballero <rcaballero082@gmail.com>
2021-10-20 09:10:02 -0600

    Primer commit y de un solo tenemos la app 100% funcional : )

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import logo from './logo.svg';
+import logo from './WiFi.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <p>Hola mundo!!!</p>
+      <p>
+          Si funciona, es por que lo hice bien y por que tienes Wi-Fi : )
         </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <br></br>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href=""
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Este Hola Mundo fue hecho con React
         </a>
       </header>
     </div>
